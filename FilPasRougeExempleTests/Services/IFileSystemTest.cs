324b02688b617d2ad189ff606dd58b4f74e5b03e
François DORIN <francois@custom-dev.fr>
2020-10-15 22:03:56 +0200

    Test unitaire pour IFileSystem

--- FilPasRougeExempleTests/Services/IFileSystemTest.cs
+++ FilPasRougeExempleTests/Services/IFileSystemTest.cs
@@ -60,6 +60,13 @@ namespace FilPasRougeExempleTests.Services
 		}
 
 		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CheckIfExistsWhenArgumentEmpty()
+		{
+			bool fileExist = this.FileSystem.Exists(null);
+		}
+
+		[TestMethod]
 		public void CheckCreateDirectory()
 		{
 			// arrange
@@ -79,6 +86,84 @@ namespace FilPasRougeExempleTests.Services
 
 			// assert
 			Assert.IsTrue(exists);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CheckCreateDirectoryRaiseExceptionWhenEmpty()
+		{
+			this.FileSystem.CreateDirectory(null);
+		}
+
+		[TestMethod]
+		[DeploymentItem("../../../Datas/BusinessLayer/VDM/viedemerde.html")]
+		public void CheckReadAllText()
+		{
+			string text = this.FileSystem.ReadAllText("viedemerde.html");
+
+			Assert.IsFalse(String.IsNullOrEmpty(text));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CheckReadAllTextWithException()
+		{
+			string text = this.FileSystem.ReadAllText(String.Empty);
+		}
+
+		[TestMethod]
+		[DeploymentItem("../../../Datas/BusinessLayer/VDM/viedemerde.html")]
+		public void CheckReadAllBytes()
+		{
+			byte[] data = this.FileSystem.ReadAllBytes("viedemerde.html");
+
+			Assert.IsNotNull(data);
+			Assert.IsTrue(data.Length > 0);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CheckReadAllBytesWithException()
+		{
+			byte[] text = this.FileSystem.ReadAllBytes(String.Empty);
+		}
+
+		[TestMethod]
+		public void CheckWriteAllText()
+		{
+			string filename = "test.txt";
+			string test = "Hello World";
+			this.FileSystem.WriteAllText(filename, test);
+
+			Assert.IsTrue(File.Exists(filename));
+			Assert.AreEqual(test, File.ReadAllText(filename));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CheckWriteAllTextWithException()
+		{
+			string test = "Hello World";
+			this.FileSystem.WriteAllText(String.Empty, test);
+		}
+
+		[TestMethod]
+		public void CheckWriteAllBytes()
+		{
+			string filename = "test.txt";
+			byte[] test = new byte[] { 0, 1, 2 };
+			this.FileSystem.WriteAllBytes(filename, test);
+
+			Assert.IsTrue(File.Exists(filename));
+			Assert.AreEqual(test, File.ReadAllText(filename));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void CheckWriteAllBytesWithException()
+		{
+			byte[] test = new byte[] { 0, 1, 2 };
+			this.FileSystem.WriteAllBytes(String.Empty, test);
 		}
 	}
 }
